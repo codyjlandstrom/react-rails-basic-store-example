fa10965ba4b85053ab3cb2380e4ef084c932ad93
Cody J Landstrom <cody.landstrom@gmail.com>
2016-06-23 19:02:45 -0400

    add controllers and route for product API from Rails

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::ItemsController < Api::V1::BaseController
+	def index
+		respond_with Product.all
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:id, :name, :description, :price, :image)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  # Route Controllers for JSON API
+  namespace :api do
+    namespace :v1 do
+      resources :products, only: [:index]
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
