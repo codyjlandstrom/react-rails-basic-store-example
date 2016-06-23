632dd21ff37ceb62ded2efa6d55cd091e04cc921
Cody J Landstrom <cody.landstrom@gmail.com>
2016-06-23 19:07:52 -0400

    fix typo

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,10 +1,9 @@
-class Api::V1::ItemsController < Api::V1::BaseController
+class Api::V1::ProductsController < Api::V1::BaseController
 	def index
 		respond_with Product.all
 	end
 
 	private
-
 	def product_params
 		params.require(:product).permit(:id, :name, :description, :price, :image)
 	end
